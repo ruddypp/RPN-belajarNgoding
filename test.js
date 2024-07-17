e71f3d3cf21855950cb709451230fac4b1ec98ad
Rudy Paningal <paningalrudy@gmail.com>
2024-07-18 00:43:38 +0700

    Push Pertama

---
+++ test.js
@@ -0,0 +1,44 @@
+console.log('hello world!')
+
+let angka1 = 10;
+let angka2 = 20;
+
+console.log(angka1 + angka2, 'Pertambahan');
+console.log(angka1 - angka2, 'Pengurangan');
+console.log(angka1 * angka2, 'Perkalian' );
+console.log(angka1 / angka2, 'Pembagian' );
+console.log(angka1 % angka2, 'Sisabagi' );
+
+// Camelcase
+
+let aritmatikaRPN = angka1 + angka2;
+console.log(aritmatikaRPN);
+
+aritmatikaRPN = angka1 - angka2;
+console.log(aritmatikaRPN);
+
+aritmatikaRPN = angka1 * angka2;
+console.log(aritmatikaRPN);
+
+aritmatikaRPN = angka1 / angka2;
+console.log(aritmatikaRPN);
+
+aritmatikaRPN = angka1 % angka2;
+console.log(aritmatikaRPN);
+
+const namaDepan = 'rudy';
+const namaBelakang = 'paningal';
+console.log ( namaDepan + ' ' + namaBelakang);
+
+const namaLengkap = namaDepan + namaBelakang;
+console.log(namaLengkap);
+
+
+// logic
+
+let number = 10;
+console.log(number + 10);
+number = false;
+console.log(number + 20);
+number = true;
+console.log(number + 20);
